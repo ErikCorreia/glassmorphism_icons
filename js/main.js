38c6ace3d2a8a7b9604e34bd86f6989ae8735c85
Erik Correia <erickcorreia727@gmail.com>
2021-08-14 01:46:51 -0400

    reupload de arquivos para um novo repositorio

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const colors = document.querySelector('#section');
+
+const facebook = document.querySelector('#facebook');
+const instagram = document.querySelector('#instagram');
+const whatsapp = document.querySelector('#whatsapp');
+const github = document.querySelector('#github');
+
+colors.addEventListener('mouseout', () => {
+  colors.style.background = 'linear-gradient(to bottom, #0cc1c7, #00b862)';
+});
+
+/*Altera a cor de fundo do elemento "sectin" quando o evento de "mouseover" for disparado
+para cada icone */
+facebook.addEventListener('mouseover', () => {
+  colors.style.background = '#0055c4';
+});
+whatsapp.addEventListener('mouseover', () => {
+  colors.style.background = '#159e43';
+});
+instagram.addEventListener('mouseover', () => {
+  colors.style.background = 'linear-gradient(rgb(143, 6, 47), rgb(165, 65, 40))';
+});
+github.addEventListener('mouseover', () => {
+  colors.style.background = '#22272e';
+});
